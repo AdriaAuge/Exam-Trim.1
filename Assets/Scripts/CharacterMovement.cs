acd4f9a55eddef694b82cbc5b420e54ee7885549
AdriaAuge <79134252+AdriaAuge@users.noreply.github.com>
2021-10-27 10:12:56 +0200

    Exam

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public float horizontal;
+    public float vertical;
     public float speed;
-
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
 
     public CharacterController player;
 
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        if (direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            player.Move(direction * speed * Time.deltaTime);
-        }
+    private void FixedUpdate()
+    {
+        player.Move(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
     }
 }
+
