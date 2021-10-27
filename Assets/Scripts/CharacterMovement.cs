2b8be22044dd1f226a99d665d00c5819b6aa4b62
AdriaAuge <79134252+AdriaAuge@users.noreply.github.com>
2021-10-27 11:01:36 +0200

    Exam

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public float horizontal;
-    public float vertical;
     public float speed;
+    public float gravity;
+    private float fallVelocity;
 
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public Transform cam;
     public CharacterController player;
-
-    void Start()
-    {
-        player = GetComponent<CharacterController>();
-    }
 
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-    }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-    private void FixedUpdate()
-    {
-        player.Move(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            player.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
     }
 }
-
